213c1163c5b36457337ddc6bf523d1ac6a2c1a7c
Shainy22 <Karnatishainy22@gmail.com>
2025-04-15 15:57:03 +0530

    Testing 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://collaborative-env.vercel.app");
 const CLEAR_PASSWORD = import.meta.env.VITE_CLEAR_PASSWORD;
 
 function App() {
